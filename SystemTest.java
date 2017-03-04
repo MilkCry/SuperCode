a0c967140bbb13ae6a6d090fc07cfde30f651fbe
MilkCry <yoursmileisallofme@qq.com>
2017-03-04 10:26:13 +0800

    Update SystemTest.java

--- SystemTest.java
+++ SystemTest.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException; 
-import java.io.InputStreamReader; /**  * Created by Milk on 2017/3/1.  */ 
+import java.io.InputStreamReader;  
 	public class SystemInTest 
 	{ 
 		public static void main(String[] args) 
